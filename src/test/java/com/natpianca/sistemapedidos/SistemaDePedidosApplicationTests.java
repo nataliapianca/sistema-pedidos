5f931da05a82f91abe362f46d474beb54869f491
nataliapianca <88843341+nataliapianca@users.noreply.github.com>
2025-12-13 10:49:41 -0300

    project created

---
+++ src/test/java/com/natpianca/sistemapedidos/SistemaDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natpianca.sistemapedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
